e862b57f4e9dd29c78e9cb3a0fa35e4356cbfa9d
natarajanraja10140 <109866285+natarajanraja10140@users.noreply.github.com>
2022-09-29 10:36:40 -0700

    Add files via upload

---
+++ Input Functions.py
@@ -0,0 +1,11 @@
+#input functions
+
+a=int(input("Number1: "))
+b=int(input("Number2: "))
+c=a+b
+print(c)
+
+#string as input
+
+Name=input("Enter your Name: ")
+print("Hello",Name)
---
+++ Print Functions .py
@@ -0,0 +1,8 @@
+#print in python
+
+print("I love Codings")
+print("Jesus I Love You")
+a=10
+b=20
+c=a+b
+print(c)
